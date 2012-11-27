3fc24ad28da9048cdab52e47abd8749139d14ad0
Alejandro Acosta-Rubio <aacostarubio@gmail.com>
2012-11-26 18:03:31 -0600

    idk

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ before_filter :require_user, :except => [:new, :create]
   # POST /users.json
   def create
     @user = User.new(params[:user])
-    (@user.save) ? (session[:user_id] = @user.id ; redirect_to(instagram_access_url) ) : (redirect_to(new_user_url) ;   flash[:notice] = "User exists!!!")
+    (@user.save) ? (session[:user_id] = @user.id ; redirect_to instagram_access_url) ) : (redirect_to(new_user_url) ;   flash[:notice] = "User exists!!!")
 
   end
 
