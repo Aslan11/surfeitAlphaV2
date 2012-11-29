f175947daa1bb7c81b0e02979c94f1a8c199c3ad
Alejandro Acosta-Rubio <aacostarubio@gmail.com>
2012-11-29 17:44:17 -0600

    refactoring pages_controller current action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,27 +1,15 @@
 class PagesController < ApplicationController
 
-  def instagram
-    redirect_to :controller => 'sessions', :action => 'connect' if !session[:access_token] 
-    client = Instagram.client(:access_token => session[:access_token])
-    @instagram_user = client.user
-    @recent_media_items = client.user_recent_media
-  end
-  
+  before_filter :authenticate_user!, :only => [:current]
+
   def index
     redirect_to user_current_path if session[:user_id]
   end
 
   def current
-    if !session[:user_id]   # no one is logged in
-      redirect_to index_path, :notice => "You must sign in first"
-    end  
-    
-    redirect_to :controller => 'sessions', :action => 'connect' if !session[:access_token] 
+    redirect_to :controller => 'sessions', :action => 'connect' unless session[:access_token] 
     client = Instagram.client(:access_token => session[:access_token])
-    @instagram_user = client.user
-    @recent_media_items = client.user_media_feed
     @mediafeed = client.user_media_feed
-    @user = User.find(session[:user_id])  
 
     if params[:code]
       session[:access_token] = session[:oauth].get_access_token(params[:code])
@@ -31,4 +19,10 @@ class PagesController < ApplicationController
     @graph_data = @api.get_object("/me/home")
   end
     
+  private
+
+  def authenticate_user!
+    redirect_to index_path, notice: "You must sign in first" unless session[:user_id]
+  end
+
 end
