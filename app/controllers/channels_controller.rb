24914f884e5e8e87d24de148530bd351101ca7fa
Alejandro Acosta-Rubio <aacostarubio@gmail.com>
2012-11-29 17:27:44 -0600

    adding pry to the Gemfile and refactoring channels controller

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,11 +1,6 @@
 class ChannelsController < ApplicationController
   
   def index
-  	@user = User.find(session[:user_id])
-  	session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/current')
-	@auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream")
-	puts session.to_s + "<<< session"
   end
-
 
 end
