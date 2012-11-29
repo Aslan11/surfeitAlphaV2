24daeb101b4d56e1dc24469d1a6b6e67d6a66df5
Alejandro Acosta-Rubio <aacostarubio@gmail.com>
2012-11-29 17:12:46 -0600

    fixing indentation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,51 +1,34 @@
 class PagesController < ApplicationController
 
-
   def instagram
-
-  redirect_to :controller => 'sessions', :action => 'connect' if !session[:access_token] 
-
-  client = Instagram.client(:access_token => session[:access_token])
+    redirect_to :controller => 'sessions', :action => 'connect' if !session[:access_token] 
+    client = Instagram.client(:access_token => session[:access_token])
     @instagram_user = client.user
     @recent_media_items = client.user_recent_media
-    
   end
   
   def index
-  	if session[:user_id]   # someone is already logged in
-     	# redirect_to user_current_path
-      puts "Logged in as #{session[:email]}"
-  	 end
+    redirect_to user_current_path if session[:user_id]
   end
 
-
-
   def current
-
-     	 if !session[:user_id]   # no one is logged in
-        	redirect_to index_path, :notice => "You must sign in first"
-     	 end	 
+    if !session[:user_id]   # no one is logged in
+      redirect_to index_path, :notice => "You must sign in first"
+    end  
     
-     redirect_to :controller => 'sessions', :action => 'connect' if !session[:access_token] 
-    
-     client = Instagram.client(:access_token => session[:access_token])
-       @instagram_user = client.user
-       @recent_media_items = client.user_media_feed
-       @mediafeed = client.user_media_feed
-       
-       @user = User.find(session[:user_id])  
+    redirect_to :controller => 'sessions', :action => 'connect' if !session[:access_token] 
+    client = Instagram.client(:access_token => session[:access_token])
+    @instagram_user = client.user
+    @recent_media_items = client.user_media_feed
+    @mediafeed = client.user_media_feed
+    @user = User.find(session[:user_id])  
 
     if params[:code]
-      # acknowledge code and get access token from FB
       session[:access_token] = session[:oauth].get_access_token(params[:code])
     end   
 
-     # auth established, now do a graph call:
-      
     @api = Koala::Facebook::API.new(session[:access_token])
     @graph_data = @api.get_object("/me/home")
-    
-    
-     end
+  end
     
 end
