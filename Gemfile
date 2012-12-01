bfb01ed5033bccbe14b2c60d2266286fbe2a02a8
Alejandro Acosta-Rubio <aacostarubio@gmail.com>
2012-11-30 18:56:23 -0600

    boom

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'sqlite3'
 
 gem 'instagram'
 
+# gem 'feedzirra'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,25 +1,39 @@
 class PagesController < ApplicationController
 
   before_filter :authenticate_user!, :only => [:current]
+  before_filter :instagram, :only => [:current]
 
   def index
     redirect_to instagram_access_url if session[:user_id]
   end
 
   def current
-    redirect_to :controller => 'sessions', :action => 'connect' unless session[:access_token] 
-    client = Instagram.client(:access_token => session[:access_token])
-    @mediafeed = client.user_media_feed
+    # binding.pry
+    if params[:code]
+    session[:access_token] = session[:oauth].get_access_token(params[:code])
+    end   
 
     # binding.pry
-
-    if params[:code]
-      session[:access_token] = session[:oauth].get_access_token(params[:code])
-    end   
 
     @api = Koala::Facebook::API.new(session[:access_token])
     @graph_data = @api.get_object("/me/home")
   end
+
+  def instagram
+
+    redirect_to :controller => 'sessions', :action => 'connect' unless session[:access_token] 
+    client = Instagram.client(:access_token => session[:access_token])
+    @mediafeed = client.user_media_feed
+    
+  end
+
+  def facebook
+
+
+    
+  end
+
+
     
   private
 
