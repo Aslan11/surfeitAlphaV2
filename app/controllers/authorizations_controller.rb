7525e44cb5e512c997b8dbf5cb9c3225aa22a8fc
Alejandro Acosta-Rubio <aacostarubio@gmail.com>
2012-12-03 22:44:04 -0600

    all apis working.. working on create new user

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -11,7 +11,7 @@ class AuthorizationsController < ApplicationController
  	def instagram
 	  response = Instagram.get_access_token(params[:code], redirect_uri: authorize_instagram_url)
 	  
-	  AccessToken.forst_or_create(
+	  AccessToken.create(
 	  	access_token: response.access_token, 
 	  	user_id: current_user.id, 
 	  	service: 'instagram'
@@ -23,7 +23,7 @@ class AuthorizationsController < ApplicationController
  	def facebook
 		response = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, authorize_facebook_url)
 		
-		 AccessToken.first_or_create(
+		 AccessToken.create(
 	  	access_token: response.get_access_token(params[:code]), 
 	  	user_id: current_user.id, 
 	  	service: 'facebook'
@@ -34,8 +34,8 @@ class AuthorizationsController < ApplicationController
 
   def twitter
   	response = env['omniauth.auth']
-
-	  AccessToken.first_or_create(
+  	
+	  AccessToken.create(
 	  	access_token: response.credentials.token,
 	  	access_token_secret: response.credentials.secret, 
 	  	user_id: current_user.id, 
