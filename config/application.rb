d62d0e984dc9dd7777378d67901096a13589a8ec
Kevin Martin <kvn.p.mrtn@gmail.com>
2012-12-13 19:34:34 -0600

    yee

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module Surfeitalpha
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initiazlize_on_precompile = false 
+    #config.assets.initiazlize_on_precompile = false 
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
